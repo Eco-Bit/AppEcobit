9379dfc9fde16dc0ddafa8cfd7f82f53ceb45acf
EuLucasDavi <lucas._.davi@hotmail.com>
2024-10-17 03:09:36 -0300

    btn's voltar, Profile OK, padrão nos Java

---
+++ app/src/main/java/br/com/fatec/ecobit/ProfileActivity.java
@@ -0,0 +1,77 @@
+package br.com.fatec.ecobit;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProfileActivity extends AppCompatActivity {
+
+    private ImageView btnVoltar;
+    private Button btnCancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        initializeComponents();
+
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_profile);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        btnVoltar.setOnClickListener(v -> finish());
+        btnCancel.setOnClickListener(v -> finish());
+    }
+
+    public void initializeComponents(){
+        btnVoltar = findViewById(R.id.btnBack);
+        btnCancel = findViewById(R.id.btnProCancel);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Ciclo de vida Profile", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Ciclo de vida Profile", "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Ciclo de vida Profile", "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Ciclo de vida Profile", "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Ciclo de vida Profile", "onDestroy");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Ciclo de vida Profile", "onPause");
+    }
+}
